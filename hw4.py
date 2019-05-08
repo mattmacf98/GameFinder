23d6cc032d70bc986b79643edca343e98d5852f3
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2019-05-08 16:08:10 -0400

    add configuration for newegg

--- hw4.py
+++ hw4.py
@@ -39,75 +39,6 @@ def get_video_games_ebay(url, html, game):
             results.append(res)
     return results
 
-#GAMESTOP
-def getInfoFromCard_gameStop(listItem, game):
-    soup = BeautifulSoup(listItem, 'html.parser')
-
-    #get the header for the card whichc contains the link and title
-    header = soup.find("h3", {"class":"ats-product-title"})
-    header_str = str(header)
-
-    link_soup = BeautifulSoup(header_str, 'html.parser')
-    title = link_soup.find('a').text
-    title_matcher = re.compile(r"{0,20}" + re.escape(game) + r".{0,40}</h3>")
-    if not title_matcher.match(title):
-        #if the title does not match the regex, do not include this game in the results
-        return
-
-    #now get the link href
-    link = link_soup.find('a')['href']
-
-    price = 0
-    price = soup.find('p', {"class": "ats-product-price"}).text
-
-    if price is not 0 and title is not '' and link is not '':
-        return (title, float(price[1:]), link)
-
-def get_video_games_gameStop(url, html, game):
-    results = []
-    soup = BeautifulSoup(html, 'html.parser')
-    for listItem in soup.find_all("div", {"class": "product"}):
-        res = getInfoFromCard_gameStop(str(listItem), game)     
-        if res is not None:
-            results.append(res)
-    return results
-
-#WALMART
-def getInfoFromCard_walmart(listItem, game):
-    soup = BeautifulSoup(listItem, 'html.parser')
-
-    #get the header for the card whichc contains the link and title
-    header = soup.find("a", {"class":"product-title-link"})
-    title = header['title']
-    title_matcher = re.compile(r"{0,20}" + re.escape(game) + r".{0,40}</h3>")
-    if not title_matcher.match(title):
-        #if the title does not match the regex, do not include this game in the results
-        return
-
-    #now get the link href
-    link = header['href']
-
-    price = 0
-    price_span = soup.find('span', {"class": "price-group"})
-    price = price_span['aria-label']
-
-    if price is not 0 and title is not '' and link is not '':
-        return (title, float(price[1:]), link)
-
-def get_video_games_walmart(url, html, game):
-    results = []
-    soup = BeautifulSoup(html, 'html.parser')
-    print(soup.prettify())
-    group = soup.find("div", {"id":"searchProductResult"})
-    list_soup = BeautifulSoup(str(group), 'html.parser')
-    for listItem in list_soup.find("li"):
-        print(listItem)
-        break
-        res = getInfoFromCard_walmart(str(listItem), game)     
-        if res is not None:
-            results.append(res)
-    return results
-
 #DEEPDISCOUNT
 def getInfoFromCard_deep_discount(listItem, game):
     soup = BeautifulSoup(listItem, 'html.parser')
@@ -123,7 +54,7 @@ def getInfoFromCard_deep_discount(listItem, game):
         return
     title = title_sp.text
     title_matcher = re.compile(r".{0,20}" + re.escape(game) + r".{0,40}")
-    if not title_matcher.match(title):
+    if not title_matcher.match(title) or title.index(game) + len(game) < len(title) - 40:
         #if the title does not match the regex, do not include this game in the results
         return
 
@@ -153,8 +84,40 @@ def get_video_games_deep_discount(url, html, game):
             results.append(res)
     return results
 
+#NEWEGG
+def getInfoFromCard_new_egg(listItem, game):
+    soup = BeautifulSoup(listItem, 'html.parser')
 
+    #get the header for the card whichc contains the link and title
+    header = soup.find("a", {"class":"item-title"})
+    title = header.text
+    link = header['href']
 
+    title_matcher = re.compile(r".{0,20}" + re.escape(game) + r".{0,40}")
+    if not title_matcher.match(title) or title.index(game) + len(game) < len(title) - 40:
+        #if the title does not match the regex, do not include this game in the results
+        return
+
+    price_group = soup.find('li', {"class":"price-current"})
+    if price_group is None:
+        return
+
+    price = 0
+    price_soup = BeautifulSoup(str(price_group), 'html.parser')
+    price = float(price_soup.find('strong').text)
+    price = price + float(price_soup.find('sup').text)
+
+    if price is not 0 and title is not '' and link is not '':
+        return (title, price, link)
+
+def get_video_games_new_egg(url, html, game):
+    results = []
+    soup = BeautifulSoup(html, 'html.parser')
+    for listItem in soup.find_all("div", {"class":"item-container"}):
+        res = getInfoFromCard_new_egg(str(listItem), game)     
+        if res is not None:
+            results.append(res)
+    return results
 
 if __name__ == '__main__':
     #main()
@@ -165,30 +128,24 @@ if __name__ == '__main__':
     html = req.read()
     ebay_results = get_video_games_ebay(urlEbay, html, game)
 
-    #urlGameStop = "https://www.gamestop.com/browse?nav=16k-" + game.replace(" ","+")
-    #req = request.urlopen(urlGameStop)
-    #html =  req.read()
-    #gameStop_results = get_video_games_gameStop(urlGameStop, html, game)
-
-    #urlBestBuy = "https://www.bestbuy.com/site/searchpage.jsp?st=" + game.replace(" ","+")
-    #req = request.urlopen(urlBestBuy)
-    #html = req.read()
-
-    #urlWalmart = "https://www.walmart.com/search/?query=" + game.replace(" ", "%20")
-    #req =  request.urlopen(urlWalmart)
-    #html =  req.read()
-    #walmart_results =  get_video_games_walmart(urlWalmart, html, game)
-
     urlDeepDiscount = "https://www.deepdiscount.com/search?q="+game.replace(" ", "+")
     req = request.urlopen(urlDeepDiscount)
     html = req.read()
     deepdiscount_results =  get_video_games_deep_discount(urlDeepDiscount, html, game)
+
+    urlNewEgg = "https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=" + game.replace(" ", "+")
+    req = request.urlopen(urlNewEgg)
+    html = req.read()
+    newEgg_results =  get_video_games_new_egg(urlNewEgg, html, game)
+
     
     results = []
     for i in ebay_results:
         results.append(i)
     for i in deepdiscount_results:
         results.append(i)
+    for i in newEgg_results:
+        results.append(i) 
 
 
     results.sort(key=lambda x:x[1])
@@ -196,4 +153,4 @@ if __name__ == '__main__':
 
 
     for i in results:
-        print(i)
+       print(i)
