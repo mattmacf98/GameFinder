4cd1cf4b6238d621c955bd9166ca6fbbe6f3635e
jmill269 <jmill2698@gmail.com>
2019-05-12 21:05:52 -0400

    tiny tiny code clean up and prepping for merge

--- hw4.py
+++ hw4.py
@@ -1,21 +1,21 @@
 import re
 import sys
 from bs4 import BeautifulSoup
-from urllib import parse, request
-import time
+from urllib import request
+
 
 #EBAY
 def getInfoFromCard_ebay(listItem, game):
     soup = BeautifulSoup(listItem, 'html.parser')
 
     string = listItem.lower()
-    string = re.sub(r"[,:\.!\?']","",string)
+    string = re.sub(r"[,:\.!\?']", "", string)
 
     title_matcher = re.compile(r"<h3 class=\"s-item__title\">.{0,20}" + re.escape(game) + r".{0,40}</h3>")
     t = title_matcher.search(string)
     title = ""
     if t is not None:
-        #take away the garbage from the title
+        # take away the garbage from the title
         title  = t.group(0)
         title = title[0:title.index('</h3>')]
         title = title[title.index('\">')+ 2:]
@@ -23,14 +23,15 @@ def getInfoFromCard_ebay(listItem, game):
     price = 0
     price = soup.find('span', {"class": "s-item__price"}).text
     if len(price) > 6:
-        #sometimes need to click to see the price :(
+        # sometimes need to click to see the price :(
         return
 
     link = ''
     link = soup.find('a', {"class": "s-item__link"})['href']
 
     if price is not 0 and title is not '' and link is not '':
-        return (title, float(price[1:]), link)
+        return title, float(price[1:]), link
+
 
 def get_video_games_ebay(url, html, game):
     results = []
@@ -41,12 +42,13 @@ def get_video_games_ebay(url, html, game):
             results.append(res)
     return results
 
+
 #DEEPDISCOUNT
 def getInfoFromCard_deep_discount(listItem, game):
     soup = BeautifulSoup(listItem, 'html.parser')
 
-    #get the header for the card whichc contains the link and title
-    header = soup.find("a", {"class":"aec-listlink"})
+    # get the header for the card whichc contains the link and title
+    header = soup.find("a", {"class": "aec-listlink"})
     header_str = str(header)
 
     link_soup = BeautifulSoup(header_str, 'html.parser')
@@ -56,13 +58,13 @@ def getInfoFromCard_deep_discount(listItem, game):
         return
     title = title_sp.text
     title = title.lower()
-    title = re.sub(r"[,:\.!\?']","",title)
+    title = re.sub(r"[,:\.!\?']", "", title)
     title_matcher = re.compile(r".{0,20}" + re.escape(game) + r".{0,40}")
     if not title_matcher.match(title) or title.index(game) + len(game) < len(title) - 40:
-        #if the title does not match the regex, do not include this game in the results
+        # if the title does not match the regex, do not include this game in the results
         return
 
-    #now get the link href
+    # now get the link href
     link = "https://www.deepdiscount.com" + link_soup.find('a')['href']
 
     price_group = soup.find('div', {"class":"aec-custprice"})
@@ -79,7 +81,7 @@ def getInfoFromCard_deep_discount(listItem, game):
 def get_video_games_deep_discount(url, html, game):
     results = []
     soup = BeautifulSoup(html, 'html.parser')
-    group = soup.find("ul", {"id":"aec-prodgrid"})
+    group = soup.find("ul", {"id": "aec-prodgrid"})
     
     li_soup = BeautifulSoup(str(group), 'html.parser')
     for listItem in li_soup.findChildren("li"):
@@ -88,23 +90,23 @@ def get_video_games_deep_discount(url, html, game):
             results.append(res)
     return results
 
-#NEWEGG
+# NEWEGG
 def getInfoFromCard_new_egg(listItem, game):
     soup = BeautifulSoup(listItem, 'html.parser')
 
-    #get the header for the card whichc contains the link and title
+    # get the header for the card whichc contains the link and title
     header = soup.find("a", {"class":"item-title"})
     title = header.text
     title = title.lower()
-    title = re.sub(r"[,:\.!\?']","",title)
+    title = re.sub(r"[,:\.!\?']", "", title)
     link = header['href']
 
     title_matcher = re.compile(r".{0,20}" + re.escape(game) + r".{0,40}")
     if not title_matcher.match(title) or title.index(game) + len(game) < len(title) - 40:
-        #if the title does not match the regex, do not include this game in the results
+        # if the title does not match the regex, do not include this game in the results
         return
 
-    price_group = soup.find('li', {"class":"price-current"})
+    price_group = soup.find('li', {"class": "price-current"})
     if price_group is None:
         return
 
@@ -114,7 +116,8 @@ def getInfoFromCard_new_egg(listItem, game):
     price = price + float(price_soup.find('sup').text)
 
     if price is not 0 and title is not '' and link is not '':
-        return (title, price, link)
+        return title, price, link
+
 
 def get_video_games_new_egg(url, html, game):
     results = []
@@ -125,15 +128,16 @@ def get_video_games_new_egg(url, html, game):
             results.append(res)
     return results
 
-#GAMEOVERGAMES
+
+# GAMEOVERGAMES
 def getInfoFromCard_game_over_games(listItem, game):
     soup = BeautifulSoup(listItem, 'html.parser')
 
-    #get the title and price from the div components
+    # get the title and price from the div components
     header = soup.find("div", {"class":"element"})
     title = header['data-alpha']
     title = title.lower()
-    title = re.sub(r"[,:\.!\?']","",title)
+    title = re.sub(r"[,:\.!\?']", "", title)
     price = header['data-price']
     price = float(price)/100
     
@@ -162,7 +166,7 @@ if __name__ == '__main__':
     #main()
     game = sys.argv[1]
     game = game.lower()
-    game = re.sub(r"[,:\.!\?']","",game)
+    game = re.sub(r"[,:\.!\?']", "", game)
 
     urlEbay="https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + game.replace(" ","+") + "&LH_BIN=1"
     req = request.urlopen(urlEbay)
